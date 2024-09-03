4f175724a4ba72a7cbdf65521542aaa560415072
gus <gustav1105@gmail.com>
2024-09-03 13:58:49 +0200

    users get profile and locale

--- lib/user_database/users.ex
+++ lib/user_database/users.ex
@@ -1,9 +1,24 @@
 defmodule UserDatabase.Users do
-  alias UserDatabase.{Repo, Users.Profile}
+  use Ecto.Schema
+  import Ecto.Query
+
+  alias UserDatabase.{Repo, Users.Profile, Users.User}
 
   def all(params) do
     params
-    |> Enum.reduce(Profile, &convert_field_to_query/2)
+    |> Enum.reduce(User, &convert_field_to_query/2)
+    |> Repo.all()
+    |> Repo.preload([:profile, :locale])
+  end
+
+  def by_name(search_term) do
+    search_pattern = "%#{search_term}%"
+
+    User
+    |> join(:inner, [u], p in assoc(u, :profile))
+    |> join(:inner, [u, p], l in assoc(p, :locale))
+    |> where([_, p, _], ilike(p.first_name, ^search_pattern) or ilike(p.last_name, ^search_pattern))
+    |> preload([:profile, :locale])
     |> Repo.all()
   end
 
@@ -22,6 +37,5 @@ defmodule UserDatabase.Users do
   defp convert_field_to_query({:by_country, {country, post_code}}, query) do
     Profile.by_locale(query, country, post_code)
   end
-
 end
 
--- lib/user_database/users/user.ex
+++ lib/user_database/users/user.ex
@@ -1,21 +1,25 @@
 defmodule UserDatabase.Users.User do
   use Ecto.Schema
+  
   import Ecto.Changeset
+  import Ecto.Query
+
+  alias UserDatabase.{Repo, Users.Profile, Users.Locale}
 
   schema "users" do
-    belongs_to :profile, UserDatabase.Users.Profile
-    belongs_to :locale, UserDatabase.Users.Locale
+    belongs_to :profile, Profile
+    belongs_to :locale, Locale
     timestamps()
   end
 
   @required_params [:profile_id, :locale_id]
 
-  def changeset(struct = %UserDatabase.Users.User{}, params) do
+  def changeset(struct = %__MODULE__{}, params) do
     struct
     |> cast(params, @required_params)
     |> validate_required(@required_params)
     |> unique_constraint(:profile_id, name: :users_profile_id_locale_id_index)
     |> unique_constraint(:locale_id, name: :users_profile_id_locale_id_index)
-  end  
+  end
 end
 
